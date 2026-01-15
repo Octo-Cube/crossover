c00bf3ef7c4a0d8e50316f942f8b8e7333c21a43
Octo-Cube <132126411+Octo-Cube@users.noreply.github.com>
2026-01-15 22:09:46 +0100

    Update CrossOver paths and repository URLs
    I changed the location for crossover from applications to apps cos applications folder is blocked

--- patch.sh
+++ patch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CROSSOVER_MACOS_PATH="/Applications/CrossOver.app/Contents/MacOS"
-REPO_URL="https://github.com/totallynotinteresting/crossover.git"
-RAW_URL="https://raw.githubusercontent.com/totallynotinteresting/crossover/main"
-RELEASE_URL="https://github.com/totallynotinteresting/crossover/releases/latest/download/hook.dylib"
+CROSSOVER_MACOS_PATH="/Apps/CrossOver.app/Contents/MacOS"
+REPO_URL="https://github.com/Octo-Cube/crossover.git"
+RAW_URL="https://raw.githubusercontent.com/Octo-Cube/crossover/main"
+RELEASE_URL="https://github.com/Octo-Cube/crossover/releases/latest/download/hook.dylib"
 
 if [ ! -d "$CROSSOVER_MACOS_PATH" ]; then
     echo "CrossOver.app was not found at $CROSSOVER_MACOS_PATH"
